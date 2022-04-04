4c04fd2a71cc8b65b16d1a230c35def3a5350f74
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2022-04-04 23:48:23 +0530

    fix

--- index.js
+++ index.js
@@ -51,11 +51,6 @@ const options = {
 }
 http.createServer(app).listen(port, () => console.log(`Server started on port ${port}`))
 https.createServer(options, app).listen(3000, () => console.log(`Server started on port 3000`))
-app.get('*',function(req,res,next){
-    if(req.headers['x-forwarded-proto'] != 'https'){
-      res.redirect('https://platform.techsyndicate.us'+req.url);
-    } else next();
-  });
 
 // 404 page
 app.use((req, res, next) => {
