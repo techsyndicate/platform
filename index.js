ef7f0c8c867c9f47cee7dbaf38063ad9ff8f89c3
Aayush Garg <aayushgarg2006@gmail.com>
2022-04-05 14:39:08 +0530

    Prod test

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-
+const https = require('https');
+const http = require('http');
 const cookieparser = require('cookie-parser')
 require('dotenv').config()
 
@@ -17,8 +18,8 @@ app.use(cookieparser())
 app.use(express.json())
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const indexRoute = require('./routes/indexRoute')
@@ -27,21 +28,43 @@ const dashboardRoute = require('./routes/dashboardRoute')
 const adminRoute = require('./routes/adminRoute')
 const taskRoute = require('./routes/taskRoute')
 
+//cert options
+const options = {
+    key: Buffer.from(process.env.PVT_KEY, 'base64').toString(),
+    cert: Buffer.from(process.env.CERT, 'base64').toString(),
+    ca: Buffer.from(process.env.INT_CERT, 'base64').toString()
+}
+
+let httpsServer = https.createServer(options, app);
+let httpServer = http.createServer(app);
+
+app.use((req, res, next) => {
+    // if http send to https
+    if (req.headers.host.includes('localhost:5001')) {
+        return res.redirect(301, `https://localhost:5000${req.url}`)
+    }
+    next();
+})
+
 app.use(authRoute)
 app.use(indexRoute)
 app.use('/dashboard', dashboardRoute)
 app.use('/admin', adminRoute)
 app.use('/task', taskRoute)
 
-const pass = process.env.MONGO_PASS 
-const port = process.env.PORT || 5000
-mongoose.connect(`mongodb+srv://techsyndicate:${pass}@cluster0.pbyaj.mongodb.net/data?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    }).then(() => console.log('Connected to MongoDB'))
-app.listen(port, () => console.log(`Server started on port ${port}`))
-
 // 404 page
 app.use((req, res, next) => {
     res.render('404')
 })
+
+const pass = process.env.MONGO_PASS
+const port = process.env.PORT || 5000
+const port1 = process.env.PORT1 || 5001
+
+mongoose.connect(`mongodb+srv://techsyndicate:${pass}@cluster0.pbyaj.mongodb.net/data?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+
+httpsServer.listen(port, () => console.log(`Server started on port ${port}`))
+httpServer.listen(port1, () => console.log(`Server started on port ${port}`))
